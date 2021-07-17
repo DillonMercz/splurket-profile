0c825f28dd0ed64201e459e92d3eae47dcfe88c8
DillonMercz <dylanclayton3@gmail.com>
2021-07-16 20:26:26 -0400

    commit

--- U466ywr363827dy.js
+++ U466ywr363827dy.js
@@ -8,106 +8,112 @@ var reviews=[];
 var skills=[];
 var user;
 var email1;
-var userfile= getParameterByName('user');
-title=document.createElement('Title');
-title.innerText=userfile +"'s Profile | Splurket"
-document.getElementsByTagName('head')[0].appendChild(title);
-
-getuserprodata(userfile);
-firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-    // User is signed in.
-    var user = firebase.auth().currentUser;
-    //splurket@gmail.com
-    var email1 = user.email;
-    
-    /*var email = getParameterByName('user'); 
-    console.log(email)*/
-    //still splurket
-    
-    var myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-    myHeaders.append("Accept", "application/json, /;q=0.5");
-
-    var raw = JSON.stringify({
-      "stringtoencrypt": `${email1}`
-    });
-    //document.write(raw)
-    var requestOptions = {
-      method: 'POST',
-      headers: myHeaders,
-      body: raw,
-      redirect: 'follow'
-    };
-
-    fetch("https://americanrivergold.com/fluffybunnyin", requestOptions)
-      .then(function (response) {
-          return response.json();
-      })
-      .then(function (data) {
-        //should be a base64 string by now
-        getuserdata(data);
-    });
-  } else {
-    document.getElementById("avatar").style.display = "none";
-    document.getElementById("login").style.display = "block";
-    document.getElementById('cartbutton').style.top='20px';
-    var all = document.getElementsByClassName('headerdrop');
-    for (var i = 0; i < all.length; i++) {
-      all[i].style.top = '83px';
+    function getParameterByName(name, url = window.location.href) {
+            name = name.replace(/[\[\]]/g, '\\$&');
+            var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+                results = regex.exec(url);
+            if (!results) return null;
+            if (!results[2]) return '';
+            return decodeURIComponent(results[2].replace(/\+/g, ' '));
     }
-  }
-});
-function getuserdata(data) {
-    email = data.encryptedstring;
-        var docRef = db.collection("users").doc(email);
-        docRef.get().then((doc) => {
-            if (doc.exists) {
-                data1 = doc.data()
-                pro_pic = data1.pro_pic;
-                if (data1.username == "" ){
-                    user_name = data1.email
-                }
-                if (data1.username != "") {
-                    user_name = data1.username;
-                }
-                //document.getElementById("avatarpic").src = pro_pic;
-                //document.getElementById("avatarpic1").src = pro_pic;
-                document.getElementById("avatarusername1").innerText = "Signed In As: " + user_name;
-                document.getElementById("avatar").style.display = "block";
-                document.getElementById("avatarpic").style.display = "inline-block";
-                var all = document.getElementsByClassName('headerdrop');
-                for (var i = 0; i < all.length; i++) {
-                  all[i].style.top = '98px';
-                }
-            } else {
-                // doc.data() will be undefined in this case
-                console.log("No such document!");
-                console.log(doc.data())
-            }
-        }).catch((error) => {
-            console.log("Error getting document:", error);
-        });
-}
-function profilego() {
-    var localstuff1 = localStorage.getItem('usercreds');
-    var localstuff = JSON.parse(localstuff1)
-    var loginemail = btoa(localstuff.email);
-    var loginpassword = btoa(localstuff.password);
-    location.href = "https://myaccount.splurket.com/login?bunnyluck="+loginemail+"&rabbitfoot="+loginpassword
+        var name, email, photoUrl, uid, emailVerified;
 
-}
-function signout() {
-    firebase.auth().signOut().then(() => {
-        localStorage.removeItem('usercreds');
-        var all = document.getElementsByClassName('headerdrop');
-        for (var i = 0; i < all.length; i++) {
-          all[i].style.top = '83px';
+        var config = {
+            apiKey: "AIzaSyC-BUGGSsvUX8z4W1LcsJzS59yrL4__EsE",
+            authDomain: "splurket-66df1.firebaseapp.com",
+            databaseURL: "https://splurket-66df1-default-rtdb.firebaseio.com",
+            projectId: "splurket-66df1",
+            storageBucket: "splurket-66df1.appspot.com",
+            messagingSenderId: "286706779903",
+            appId: "1:286706779903:web:fd91c29319f9804e192eca",
+            measurementId: "G-QWQ2M658KL"
+          };
+        firebase.initializeApp(config);
+        const db = firebase.firestore();
+        db.settings({ timestampsInSnapshots: true }); 
+        firebase.auth().onAuthStateChanged(function(user) {
+          if (user) {
+            // User is signed in.
+            var user = firebase.auth().currentUser;
+            //splurket@gmail.com
+            var email1 = user.email;
+            
+            /*var email = getParameterByName('user'); 
+            console.log(email)*/
+            //still splurket
+            
+            var myHeaders = new Headers();
+            myHeaders.append("Content-Type", "application/json");
+            myHeaders.append("Access-Control-Allow-Origin", "*")
+            myHeaders.append("Accept", "application/json, /;q=0.5");
+
+            var raw = JSON.stringify({
+              "stringtoencrypt": `${email1}`
+            });
+            //document.write(raw)
+            var requestOptions = {
+              method: 'POST',
+              headers: myHeaders,
+              body: raw,
+              redirect: 'follow'
+            };
+
+            fetch("https://americanrivergold.com/fluffybunnyin", requestOptions)
+              .then(function (response) {
+                  return response.json();
+              })
+              .then(function (data) {
+                //should be a base64 string by now
+                getuserdata1(data);
+            });
+          } else {
+            console.log('fuck')
+            document.getElementById("avatar").style.display = "none";
+            document.getElementById("login").style.display = "block";
+          }
+        });
+        function getuserdata1(data) {
+            email = data.encryptedstring;
+                var docRef = db.collection("users").doc(email);
+                docRef.get().then((doc) => {
+                    if (doc.exists) {
+                        data1 = doc.data()
+                        pro_pic = data1.pro_pic;
+                        first_name = data1.first_name;
+                        last_name = data1.last_name;
+                        username = data1.username;
+                        email = data1.email;
+                        discord = data1.discord;
+                        company = data1.company
+                        phone = data1.phone_number
+
+                        if (data1.username == "" ){
+                            user_name = data1.email
+                        }
+                        if (data1.username != "") {
+                            user_name = data1.username;
+                        }
+                        document.getElementById("myImage").src = pro_pic;
+                        document.getElementById("avatarusername").innerText = "Profile Info For " + user_name + ":";
+                        //document.getElementById("avatarusername1").innerText = "Signed In As: " + user_name;
+                        document.getElementById("uploaded_pic").style.display = "block";
+                        document.getElementById("first_name").value = first_name;
+                        document.getElementById("last_name").value = last_name;
+                        document.getElementById("username").value = username;
+                        document.getElementById("email").value = email;
+                        document.getElementById("discord").value = discord;
+                        document.getElementById("company").value = company;
+                        document.getElementById("phone").value = phone;
+                        getuserprodata(username)
+                    } else {
+                        // doc.data() will be undefined in this case
+                        console.log("No such document!");
+                        console.log(doc.data())
+                    }
+                }).catch((error) => {
+                    console.log("Error getting document:", error);
+                });
         }
-    }).catch((error) => {
-      // An error happened.
-    });
-}
 function getuserprodata(prodata){
   var docRef = db.collection('pubusers').doc(prodata);
     docRef.get().then((doc) => {
@@ -117,22 +123,6 @@ function getuserprodata(prodata){
             var skills1=doc.Skills
             skills2=skills1.split(',');
             skills2.forEach((skill3) => {skills.push(skill3)});
-            append=document.createElement('head')
-            append.innerHTML=`<meta property="og:locale" content="en_US">
-    <meta property="og:type" content="website">
-    <meta property="og:title" content="${title}">
-    <meta property="og:description" content="${doc.tagline}">
-    <meta property="og:site_name" content="Splurket">
-    <meta property="og:image" content="${doc.image}">
-    <meta itemprop="name" content="Splurket">
-    <meta itemprop="image" content="${doc.image}">
-    <meta property="og:url" content="http://www.Splurket.com">
-    <meta name="twitter:card" content="summary_large_image">
-    <meta name="twitter:site" content="@splurket_store">
-    <meta name="twitter:description" content="${doc.tagline}">
-    <meta name="twitter:title" content="Splurket">
-    <meta name="twitter:image" content="${doc.image}">`
-    document.getElementsByTagName('head')[0].appendChild(append);
             var push_data1 = {value: false, name: `${doc.username}`, image: `${doc.image}`, location: `${doc.Location}`, education: `${doc.Education}`, date: `${doc.Date}`, tag: `${doc.tagline}`, reviewsn: reviewsint, skills: skills}
             if (user_data.includes('{')){
               var push_data = ','+push_data1;
